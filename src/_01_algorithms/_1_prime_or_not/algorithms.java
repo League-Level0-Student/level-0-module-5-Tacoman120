ecf8f3857f9560a0ff3062b45afa037cdfa2edba
Marcus Solt <marcussolt723@gmail.com>
2022-09-02 17:02:06 -0700

    algorithms

---
+++ src/_01_algorithms/_1_prime_or_not/algorithms.java
@@ -0,0 +1,18 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class algorithms {
+public static void 
+main (String[]args) {
+	boolean isPrime = false;
+	String a = JOptionPane.showInputDialog(null, "Enter a number");
+	int a2 = Integer.parseInt(a);
+	for(int i = 0; i < a2; i++) {
+		if(a2%i==0) {
+			
+		}
+		
+	}
+}
+}
