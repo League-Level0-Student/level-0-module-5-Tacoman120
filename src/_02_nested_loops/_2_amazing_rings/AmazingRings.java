e1db6aa59e3418acb624258a36dcc55006403d8e
league <league@imac-12.attlocal.net>
2022-09-30 18:28:06 -0700

    AmazingRings, loop_guantlet, snowflake
    Task-Url:

--- src/_02_nested_loops/_2_amazing_rings/AmazingRings.java
+++ src/_02_nested_loops/_2_amazing_rings/AmazingRings.java
@@ -44,17 +44,29 @@ public class AmazingRings extends PApplet {
 
     @Override
     public void settings() {
-
+size(500,300);
     }
 
     @Override
     public void setup() {
 
+noFill();
     }
 
     @Override
     public void draw() {
+    	int with = 900;
+    	  int hight = 900;
+    	  //Starting with the largest ellipse, use a for loop to draw a bullseye with ellipses.
+    	  for(int e = 0; e < 40; e++){
+    	      fill(300, 0, 0);
+    	    if(e % 2 == 1){
+    	      fill(0, 0, 0);
+    	    }
+    	          ellipse(500, 500, with, hight);
 
+    	with -= 25;
+    	hight -= 25;}    	
     }
 
     static public void main(String[] args) {
---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/loop_guantlet.java
@@ -0,0 +1,10 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class loop_guantlet {
+	public loop_guantlet() {
+		// TODO Auto-generated constructor stub
+		public static void main(String[] args) {
+			
+		}
+	}
+}
